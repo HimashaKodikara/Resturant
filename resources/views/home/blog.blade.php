e02b83b73a0fd48e8c6a3096ee2c07f3eacc7446
19840769 <kodikarah.21@uom.lk>
2025-02-09 12:06:45 +0530

    (change)

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -10,18 +10,27 @@
         <div class="tab-pane fade show active" id="foods" role="tabpanel" aria-labelledby="pills-home-tab">
             <div class="row">
                 @foreach ($data as $data)
-                <div class="col-md-4">
+                    <div class="col-md-4">
 
                         <div class="my-3 bg-transparent border card my-md-0">
-                            <img height="300" src="food_img/{{$data->image}}" alt="template by DevCRID http://www.devcrud.com/"
+                            <img height="300" src="food_img/{{ $data->image }}"
+                                alt="template by DevCRID http://www.devcrud.com/"
                                 class="rounded-0 card-img-top mg-responsive">
                             <div class="card-body">
-                                <h1 class="mb-4 text-center"><a href="#" class="badge badge-primary">{{$data->price}}</a></h1>
-                                <h4 class="pt20 pb20">{{$data->title}} </h4>
-                                <p class="text-white">{{$data->details}} </p>
+                                <h1 class="mb-4 text-center"><a href="#"
+                                        class="badge badge-primary">{{ $data->price }}</a></h1>
+                                <h4 class="pt20 pb20">{{ $data->title }} </h4>
+                                <p class="text-white">{{ $data->details }} </p>
                             </div>
+                            <form action="">
+                                <input value="1" type="number" min="1" required>
+                                <input type="submit" class="btn btn-info" value="Add to Cart">
+                            </form>
+
+                        </br></br></br>
+
                         </div>
-                </div>
+                    </div>
                 @endforeach
 
             </div>
